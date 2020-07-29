392cd7f0b80ed296c7348c88fe1a8af3feea3e59
pokiebob <ccursetjee@gmail.com>
2020-07-29 17:03:01 -0400

    optimized format

--- club-finder.py
+++ club-finder.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-clubs = requests.get('https://usatt.simplycompete.com/c/d')
+clubs = requests.get('https://usatt.simplycompete.com/c/d/?max=1000')
 clubSoup = BeautifulSoup(clubs.text, 'html.parser')
 
 data = clubSoup.find_all('script')
+
+clubList = []
 
 def findIn(thing, data):
 	for i in data:
@@ -13,5 +15,18 @@ def findIn(thing, data):
 				return content
 
 
-found = findIn('window.markerData', data).replace('window.markerData =', '')
-print(found)
+found = findIn('window.markerData', data).replace('window.markerData =', '').replace('{', '[').replace('}',']').replace('\n','').replace('      ','').replace('],','').split('[')
+
+for i in found:
+	indList = []
+	i = i.split(',')
+	for element in i:
+		if ('markerName' in element) or ('addressLine1' in element) or ('addressCityState' in element):
+			indList.append(element)
+	clubList.append(indList)
+
+for i in clubList:
+	print(i)
+	print('\n\n')
+
+print(len(clubList))
